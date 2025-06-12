9444bfa7d0fd2afb821082c5e0624e8aeec0ef87
krupa <ammukrupa4@gmail.com>
2025-06-12 20:49:36 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { io } from 'socket.io-client'
+
+
+const socket = io(import.meta.env.VITE_API_URL)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App socket={socket} />
   </React.StrictMode>,
-) 
+)
