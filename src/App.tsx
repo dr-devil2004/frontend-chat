d4aaf4d59d36ffd8bf17a1be8a7cb15e1c789c34
krupa <ammukrupa4@gmail.com>
2025-06-12 21:50:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { io, Socket } from 'socket.io-client'
 import ChatRoom from './components/ChatRoom'
 import LoginForm from './components/LoginForm'
