9e0001d4d786a1b10090ed3e846dc1c705fd17d2
krupa <ammukrupa4@gmail.com>
2025-06-12 21:33:57 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,6 @@ const socket = io(import.meta.env.VITE_API_URL)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App socket={socket} />
+    <App />
   </React.StrictMode>,
 )
